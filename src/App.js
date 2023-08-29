de573ffb98a609d0dcc5893c15469aee8a40e5fa
prosenjeetshil <prosenjeetshil@gmail.com>
2023-08-29 10:22:14 +0530

    feat: routing to components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import NavBar from './Components/NavBar';
+import AddBook from './Components/Pages/AddBook';
+import ShowBook from './Components/Pages/ShowBook';
+import UpdateBook from './Components/Pages/UpdateBook';
+import DeleteBook from './Components/Pages/DeleteBook';
 
 function App() {
   return (
     <>  
-      
+      <BrowserRouter>
+      <NavBar/>
+      <Routes>
+        <Route path='/add' element={<AddBook/>}/>
+        <Route path='/show' element={<ShowBook/>}/>
+        <Route path='/update/:id' element={<UpdateBook/>}/>
+        <Route path='/delete/:id' element={<DeleteBook/>}/>
+      </Routes>
+      </BrowserRouter>
     </>
   );
 }
